023858d2b76ef3c3a2172a06669ed9d2eaa98727
CumZone1488 <eu11111444@gmail.com>
2024-04-01 00:26:55 +0300

    feat: add font, custom icons

--- src/config.rs
+++ src/config.rs
@@ -18,15 +18,14 @@ pub const PROGRAM_NAME: PCWSTR = w!("Altccents");
 pub const PROGRAM_SITE: PCWSTR = w!("https://github.com/Clovis1444/altccents");
 // Name of the icon in resources.rc
 pub const PROGRAM_ICON_IMG: PCWSTR = w!("PROGRAM_ICON");
-// pub const POPUP_FONT: PCWSTR = w!("Georgia");
 // Font name; not .ttf file name
-pub const POPUP_FONT: PCWSTR = w!("Dingleberries");
-pub const POPUP_FONT_SIZE: i32 = 20;
+pub const POPUP_FONT: PCWSTR = w!("Inter");
+pub const POPUP_FONT_SIZE: i32 = 28;
 pub const POPUP_FONT_COLOR: COLORREF = COLORREF { 0: 0x00E0E7E9 }; // Platinum
 pub const POPUP_WINDOW_TRANSPARENT_COLOR: COLORREF = COLORREF { 0: 0x00000000 }; // black
 pub const POPUP_WINDOW_TRANSPARENCY: u8 = 255;
 // Should be even, do not make too small
-pub const POPUP_CELL_SIZE: i32 = 50;
+pub const POPUP_CELL_SIZE: i32 = 55;
 pub const POPUP_SELECT_CELL_SIZE: i32 = (POPUP_CELL_SIZE as f32 * 0.9) as i32;
 pub const POPUP_CELL_COLOR: COLORREF = COLORREF { 0: 0x002B2117 };
 pub const POPUP_SELECT_CELL_COLOR: COLORREF = COLORREF { 0: 0x0078522B };
@@ -37,9 +36,9 @@ pub const POPUP_CIRCLE_SELECTION: bool = false;
 pub const TIMER_ID: usize = 1337;
 pub const TRAY_ICON_ID: u32 = 1337;
 // Name of the icon in resources.rc
-pub const TRAY_ICON_IMG_ON: PCWSTR = w!("ON_ICON");
+pub const TRAY_ICON_IMG_ON: PCWSTR = w!("PROGRAM_ON_ICON");
 // Name of the icon in resources.rc
-pub const TRAY_ICON_IMG_OFF: PCWSTR = w!("OFF_ICON");
+pub const TRAY_ICON_IMG_OFF: PCWSTR = w!("PROGRAM_OFF_ICON");
 pub const TRAY_ICON_TIP_TEXT: &str = "Altccents";
 // Should be in range 32768..49151
 pub const TRAY_CALLBACK_MESSAGE: u32 = 33333;
